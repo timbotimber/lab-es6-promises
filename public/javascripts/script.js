457e7f5cb6899a73af81d168e1fe20c5ba45f71f
timbotimber <hitimstephens@gmail.com>
2020-08-31 15:08:50 +0200

    silly changes

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,20 +2,19 @@
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
 // for (let i = 0; i < mashPotatoes.length; i++) {
-//   //addFood(steak[i], '#steak');
+//   //addFood(steak[i], id);
 //   console.log(mashPotatoes[i])
 // }
 
-let steakIt = (steak) => {
-  addFood(steak[0], "#steak", () => {
-    // ... your code here
-    addFood(steak[1], "#steak", () => {
-      addFood(steak[2], "#steak", () => {
-        addFood(steak[3], "#steak", () => {
-          addFood(steak[4], "#steak", () => {
-            addFood(steak[5], "#steak", () => {
-              addFood(steak[6], "#steak", () => {
-                addFood(steak[7], "#steak", () => {
+let steakIt = (steak, id) => {
+  addFood(steak[0], id, () => {
+    addFood(steak[1], id, () => {
+      addFood(steak[2], id, () => {
+        addFood(steak[3], id, () => {
+          addFood(steak[4], id, () => {
+            addFood(steak[5], id, () => {
+              addFood(steak[6], id, () => {
+                addFood(steak[7], id, () => {
                   const image = document.createElement("img");
                   image.src = "public/images/steak.jpg";
                   document.querySelector("#table").appendChild(image);
@@ -29,12 +28,12 @@ let steakIt = (steak) => {
   });
 };
 
-let mashIt = (mashPotatoes) => {
-  addFood(mashPotatoes[0], "#mashPotatoes").then(() => {
-    addFood(mashPotatoes[1], "#mashPotatoes").then(() => {
-      addFood(mashPotatoes[2], "#mashPotatoes").then(() => {
-        addFood(mashPotatoes[3], "#mashPotatoes").then(() => {
-          addFood(mashPotatoes[4], "#mashPotatoes").then(() => {
+let mashIt = (mashPotatoes, id) => {
+  addFood(mashPotatoes[0], id).then(() => {
+    addFood(mashPotatoes[1], id).then(() => {
+      addFood(mashPotatoes[2], id).then(() => {
+        addFood(mashPotatoes[3], id).then(() => {
+          addFood(mashPotatoes[4], id).then(() => {
             const image = document.createElement("img");
             image.src = "public/images/mashPotatoes.jpg";
             document.querySelector("#table").appendChild(image);
@@ -45,9 +44,9 @@ let mashIt = (mashPotatoes) => {
   });
 };
 
-let sproutsIt = async (sprouts) => {
+let sproutsIt = async (sprouts, id) => {
   for (let i = 0; i < sprouts.length; i++) {
-    await addFood(sprouts[i], "#brusselSprouts");
+    await addFood(sprouts[i], id);
   }
   const image = document.createElement("img");
   image.src = "public/images/brusselSprouts.jpg";
@@ -56,10 +55,10 @@ let sproutsIt = async (sprouts) => {
 
 let dinerIsServed = () => {
   Promise.all([
-    sproutsIt(brusselSprouts),
-    steakIt(steak),
-    mashIt(mashPotatoes),
-  ]).then((values) => {
+    steakIt(steak, "#steak"),
+    mashIt(mashPotatoes, "#mashPotatoes"),
+    sproutsIt(brusselSprouts, "#brusselSprouts"),
+  ]).then(() => {
     console.log("eat up martha");
   });
 };
